3e6bd3fb0e2f20e57c6303d63f4d957eeb964fa3
bromazepam <graor95@gmail.com>
2022-04-12 17:56:54 +0200

    Create ResourceControllerTest.java

---
+++ src/test/java/com/adminportal/controller/ResourceControllerTest.java
@@ -0,0 +1,22 @@
+package com.adminportal.controller;
+
+import com.adminportal.repository.BookRepository;
+import com.adminportal.service.impl.BookServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.ConcurrentModel;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+class ResourceControllerTest {
+    
+    @Test
+    void testRemoveList() {
+        ResourceController resourceController = new ResourceController(new BookServiceImpl(mock(BookRepository.class)));
+        ArrayList<String> bookIdList = new ArrayList<>();
+        assertEquals("delete success", resourceController.removeList(bookIdList, new ConcurrentModel()));
+    }
+}
+
