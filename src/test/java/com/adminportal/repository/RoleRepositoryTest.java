f3c34a1a4ebb4d3f1b10e332dfd3096136768b10
bromazepam <graor95@gmail.com>
2022-04-15 20:03:51 +0200

    Create RoleRepositoryTest.java

---
+++ src/test/java/com/adminportal/repository/RoleRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.adminportal.repository;
+
+import com.adminportal.domain.security.Role;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+public class RoleRepositoryTest {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Test
+    void findByName() {
+        //given
+        Role role = new Role();
+        role.setRoleId(1);
+        role.setName("admin");
+        roleRepository.save(new Role());
+        //when
+        Role expected = roleRepository.findByName("admin");
+
+        //then
+        assertThat(expected.getName()).isEqualTo(role.getName());
+    }
+}
