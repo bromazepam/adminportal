f31e906c2ff1b4c588a187957fb8e6cfb6d7259f
bromazepam <graor95@gmail.com>
2023-05-14 22:52:23 +0200

    Update SecurityConfigTest.java

--- src/test/java/com/adminportal/config/SecurityConfigTest.java
+++ src/test/java/com/adminportal/config/SecurityConfigTest.java
@@ -1,15 +1,53 @@
 package com.adminportal.config;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@ContextConfiguration(classes = {SecurityConfig.class, AuthenticationManagerBuilder.class,
-        AuthenticationConfiguration.class})
-@ExtendWith(SpringExtension.class)
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+
+@WebMvcTest(SecurityConfig.class)
 class SecurityConfigTest {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    public void testPublicMatchers() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/some-public-url"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @WithMockUser
+    public void testAuthenticatedAccess() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/some-private-url"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void testLoginPage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/login"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        mockMvc.perform(formLogin("/login").user("user").password("password"))
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/"))
+                .andExpect(MockMvcResultMatchers.status().isFound());
+    }
+
+    @Test
+    public void testLogout() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/logout")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/?logout"))
+                .andExpect(MockMvcResultMatchers.status().isFound());
+    }
 }
 
